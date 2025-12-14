43fcdac71cfe92d77feee1d97d9a4ffae93efea2
Ravindra7330 <ravindarravi87@gmail.com>
2025-12-14 23:43:09 +0530

    fixed css

--- apps/user-app/components/Appbar.tsx
+++ apps/user-app/components/Appbar.tsx
@@ -5,6 +5,9 @@ import { signIn,signOut, useSession } from "next-auth/react"
 export const Appbar=()=>{
     const session = useSession()
     return <div className="bg-slate-500">
+        <h1 className="text-3xl font-bold underline text-green-800">
+      Hello world!
+    </h1>
         <button onClick={()=>{
             signIn()
         }}>Signin</button>
