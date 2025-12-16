fbd2d487b58a9e15a2d9f252c9f72190a7192072
Ravindra7330 <ravindarravi87@gmail.com>
2025-12-16 15:53:28 +0530

    added sidebar

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -4,17 +4,17 @@ interface AppbarProps{
     user?:{
         name?:string |null
     },
-    OnSignin:any,
+    OnSignin: any,
     OnSignOut:any
 }
 export const Appbar=({user,OnSignin,OnSignOut}:AppbarProps)=>{
     return <div className="flex justify-between px-4 border-b border-slate-300 ">
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col justify-center ">
           PayTM
         </div>
-        <div>
+        <div className="flex flex-col justify-center pt-2">
             <Button onClick={user?OnSignOut:OnSignin} >{user?"Log Out":"Login"}</Button>
         </div>
-        
+      
     </div>
 }
